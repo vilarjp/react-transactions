cda0ec9bc963572d1090b07d28820facfea36398
João Paulo Vilar <vilarjp@hotmail.com>
2021-02-05 14:01:36 -0300

    feat: add money and date formatters

---
+++ src/utils/formatters.js
@@ -0,0 +1,33 @@
+export function formatMoney(amount) {
+  if (isNaN(amount) || amount === null)
+    throw new Error('Invalid amount format');
+
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(amount);
+}
+
+export function formatDate(date) {
+  try {
+    const dateParsed = new Date(Date.parse(date));
+    const checkIfDateIsValid =
+      dateParsed.toISOString() === date &&
+      dateParsed.toUTCString() === new Date(date).toUTCString();
+
+    if (checkIfDateIsValid) {
+      const dateToFormat = new Date(date);
+      const formattedDDMMYYYY = new Intl.DateTimeFormat('pt-BR').format(
+        dateToFormat,
+      );
+      const formattedHours = dateToFormat.getHours();
+      const formattedMinutes = dateToFormat.getMinutes();
+
+      return `${formattedDDMMYYYY} ${formattedHours}:${formattedMinutes}`;
+    } else {
+      throw new Error('Invalid date format');
+    }
+  } catch {
+    throw new Error('Invalid date format');
+  }
+}
---
+++ src/utils/formatters.spec.js
@@ -0,0 +1,42 @@
+import { formatMoney, formatDate } from './formatters';
+
+describe('formatMoney', () => {
+  it('should format an amount to a brazilian real', () => {
+    expect(formatMoney('')).toMatchInlineSnapshot(`"R$0.00"`);
+    expect(formatMoney('1.51')).toMatchInlineSnapshot(`"R$1.51"`);
+    expect(formatMoney(0)).toMatchInlineSnapshot(`"R$0.00"`);
+    expect(formatMoney(100)).toMatchInlineSnapshot(`"R$100.00"`);
+    expect(formatMoney(2523.4)).toMatchInlineSnapshot(`"R$2,523.40"`);
+  });
+
+  it('should thrown error when a invalid amount is used', () => {
+    expect(() => formatMoney('any_wrong_amount')).toThrow(
+      Error('Invalid amount format'),
+    );
+    expect(() => formatMoney({})).toThrow(Error('Invalid amount format'));
+    expect(() => formatMoney()).toThrow(Error('Invalid amount format'));
+    expect(() => formatMoney(null)).toThrow(Error('Invalid amount format'));
+    expect(() => formatMoney(undefined)).toThrow(
+      Error('Invalid amount format'),
+    );
+  });
+});
+
+describe('formatDate', () => {
+  it('should format an ISO date to DD/MM/YYYY hh:mm format', () => {
+    expect(formatDate('2020-11-10T19:43:56.451Z')).toMatchInlineSnapshot(
+      `"11/10/2020 16:43"`,
+    );
+  });
+
+  it('should thrown error when a invalid date is used', () => {
+    expect(() => formatDate('')).toThrow(Error('Invalid date format'));
+    expect(() => formatDate({})).toThrow(Error('Invalid date format'));
+    expect(() => formatDate(null)).toThrow(Error('Invalid date format'));
+    expect(() => formatDate(undefined)).toThrow(Error('Invalid date format'));
+    expect(() => formatDate('any_wrong_date')).toThrow(
+      Error('Invalid date format'),
+    );
+    expect(() => formatDate(0)).toThrow(Error('Invalid date format'));
+  });
+});
