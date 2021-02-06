e6b6c8019bed6afd1ff8506ecde863639b98226e
João Paulo Vilar <vilarjp@hotmail.com>
2021-02-05 23:13:51 -0300

    docs: add Spinner stories

---
+++ src/components/Spinner/stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Spinner from '.';
+
+export default {
+  title: 'Spinner',
+  component: Spinner,
+};
+
+export const Default = (args) => <Spinner {...args} />;
