fbfdf2d5f9775b7e6167fb504bd2b7ecfb3c3d0d
João Paulo Vilar <vilarjp@hotmail.com>
2021-02-05 23:29:22 -0300

    perf: add lazy import to pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import TransactionsList from '../pages/TransactionsList';
-import TransactionsCreate from '../pages/TransactionsCreate';
+const TransactionsList = lazy(() => import('../pages/TransactionsList'));
+const TransactionsCreate = lazy(() =>
+  import(/* webpackPrefetch: true */ '../pages/TransactionsCreate'),
+);
 
 const Routes = () => {
   return (
     <Switch>
-      <Route path="/" exact component={TransactionsList} />
-      <Route path="/create" exact component={TransactionsCreate} />
+      <Suspense fallback={<div />}>
+        <Route path="/" exact component={TransactionsList} />
+        <Route path="/create" component={TransactionsCreate} />
+      </Suspense>
     </Switch>
   );
 };
