3dc23e9e89c9a676ba1e026ede48acf940a5fcb6
João Paulo Vilar <vilarjp@hotmail.com>
2021-02-05 23:13:37 -0300

    docs: add DotsLoading stories

---
+++ src/components/DotsLoading/stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import DotsLoading from '.';
+
+export default {
+  title: 'DotsLoading',
+  component: DotsLoading,
+};
+
+export const Default = (args) => <DotsLoading {...args} />;
