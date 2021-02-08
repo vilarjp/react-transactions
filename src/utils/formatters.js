d220c12e751d7f9f90607bcb68b772cddd9b6758
João Paulo Vilar <vilarjp@hotmail.com>
2021-02-08 02:13:36 -0300

    refactor: remove formatters unnecessary code

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -31,37 +31,3 @@ export function formatDate(date) {
     throw new Error('Invalid date format');
   }
 }
-
-export function formatCPF(cpf) {
-  if (cpf.length > 14) return cpf.slice(0, 14);
-
-  cpf = cpf.replace(/\D/g, '');
-  cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2');
-  cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2');
-  cpf = cpf.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
-
-  return cpf;
-}
-
-export function formatCreditCard(creditCard) {
-  if (creditCard.length > 19) return creditCard.slice(0, 19);
-
-  creditCard = creditCard.replace(/(\d{4})(\d)/, '$1 $2');
-
-  return creditCard;
-}
-
-export function formatCardExpiration(cardExpiration) {
-  if (cardExpiration.length > 5) return cardExpiration.slice(0, 5);
-
-  if (cardExpiration.length === 3)
-    cardExpiration = cardExpiration.replace(/(\d{2})(\d)/, '$1/$2');
-
-  return cardExpiration;
-}
-
-export function formatCVV(CVV) {
-  if (CVV.length > 3) return CVV.slice(0, 3);
-
-  return CVV;
-}
